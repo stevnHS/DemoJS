66d9dd3f1c5c020fb554c0f41997c4ae4aca950d
steven Liao <stevnliao@gmail.com>
2024-04-29 16:37:06 +0800

    Saved

--- hw1/hw01.js
+++ hw1/hw01.js
@@ -1,7 +1,7 @@
 const table = document.querySelector("#table");
-for (let i = 1; i <= 9; i++) {
+for (let j = 1; j <= 9; j++) {
   const tr = document.createElement("tr");
-  for (let j = 1; j <= 9; j++) {
+  for (let i = 1; i <= 9; i++) {
     const td = document.createElement("td");
     if (i * j >= 10) {
       td.innerHTML = `${i}*${j}=${i * j}`;
